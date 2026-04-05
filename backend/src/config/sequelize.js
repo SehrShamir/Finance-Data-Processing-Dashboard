d02063122a269229331d230d6966e49dc1b7fdff
SehrShamir <sehrshamir786@gmail.com>
2026-04-05 17:02:11 +0530

    updation

--- backend/src/config/sequelize.js
+++ backend/src/config/sequelize.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'secret',
+    password: process.env.DB_PASSWORD || '@1Shamir',
     database: process.env.DB_NAME || 'finance_dashboard',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
